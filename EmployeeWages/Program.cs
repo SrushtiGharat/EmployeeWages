a6357156870b21b4ccb09c9f3e8409e37ebe94b8
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-09-26 16:29:50 +0530

    [Srushti]Welcome Message Added

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -6,7 +6,7 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee Wage Calculation Program");
         }
     }
 }
