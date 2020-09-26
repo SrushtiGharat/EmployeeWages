4224248692a5b8b6fb6e42fa43510ae1cd1e196f
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-09-26 19:10:09 +0530

    [Srushti] Refractor.Calculate daily wage

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -8,18 +8,26 @@ namespace EmployeeWages
         {
             Console.WriteLine("Welcome to Employee Wage Calculation Program");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-
+            int empHrs = 0;
+            int empWage = 0;
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee Is Present");
+                empHrs = 8;
             }
 
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage per day :"+empWage);
+
         }
     }
 }
