3010220eb642be42a0c5bd62a2cda7b4f10b55ae
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-09-27 19:19:36 +0530

    [Srushti]Refractor.Part time employee added

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,27 +7,37 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Calculation Program");
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
+            int IS_FULL_TIME = 2;
+            int IS_PART_TIME = 1;
+            
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
+            
+            int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
-            if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee Is Present");
-                empHrs = 8;
-            }
 
+            if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present full-time");
+                empHrs = 8;
+                
+            }
+            else if(empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is present part-time");
+                empHrs = 4;
+            }
             else
             {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                Console.WriteLine("Employee is absent");
+                
             }
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage per day :"+empWage);
-
+            Console.WriteLine("Employee Wage per day :" + empWage);
+   
         }
+        
     }
 }
